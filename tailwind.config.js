dc22c69f9d6f0446b476e5b92d2e82d83252d098
Yan Carlos <urbanoyaan@gmail.com>
2022-06-14 23:04:23 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-violet': '#1E0E62',
+        'violetish': '#2F1893',
+      },
+    },
+  },
+  plugins: [],
+}
